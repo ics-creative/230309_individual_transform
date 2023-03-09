453ae3143f779ab3d24217ae63c80298eca22e22
ics-nishihara <nishihara@ics-web.jp>
2023-03-09 15:07:31 +0900

    コメントの修正

--- src/public/mouseStalker.js
+++ src/public/mouseStalker.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // マウスストーカー
-  const stalker = document.querySelector(".mouseStalker_cursor");
+  // マウスストーカーの要素
+  const stalkerElement = document.querySelector(".mouseStalker_cursor");
   // マウス右下になるよう24pxのオフセット
   const OFFSET = 24;
   document.addEventListener("mousemove", (e) => {
     // 位置をセット
-    stalker.style.translate = `${e.clientX + OFFSET}px ${e.clientY + OFFSET}px`;
+    stalkerElement.style.translate = `${e.clientX + OFFSET}px ${e.clientY + OFFSET}px`;
   });
-  const interactive = document.querySelectorAll("a,button");
-  interactive.forEach((element) => {
+  const interactiveElements = document.querySelectorAll("a,button");
+  interactiveElements.forEach((element) => {
     // インタラクティブなコンテンツにホバーしたときにアクティブクラスを付与する
     element.addEventListener("mouseenter", () => {
-      stalker.classList.add("active");
+      stalkerElement.classList.add("active");
     });
+    // インタラクティブなコンテンツから出たときにアクティブクラスを削除する
     element.addEventListener("mouseleave", () => {
-      stalker.classList.remove("active");
+      stalkerElement.classList.remove("active");
     });
   });
 });
